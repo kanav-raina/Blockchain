da830bc6c083179e157ef78e7b563b49468cd845
KIRA <kanavraina98@gmail.com>
2019-07-15 20:55:00 +0530

    Update kitcoin.py

--- kitcoin.py
+++ kitcoin.py
@@ -17,6 +17,7 @@ class Blockchain:
         self.chain=[]
         self.transactions=[]
         self.create_block(proof=1,previous_hash='0')
+        self.nodes=set()
         
     def create_block(self,proof,previous_hash):
         block={"index":len(self.chain)+1 ,
@@ -67,10 +68,34 @@ class Blockchain:
                                   'amount':amount})
         previous_block=self.get_previous_block()
         return previous_block['index']+1
+    
+    def add_node(self,address):
+        parsed_url=urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+        
+    def replace_chain(self):
+        network=self.nodes
+        longest_chain=None
+        max_length=len(self.chain)
+        for node in network:
+            response=requests.get(f'http://{node}/get_chain')
+            if response.status_code==200:
+                length=response.json()['length']
+                chain=response.json()['chain']
+                if length>max_length and self.is_chain_calid(chain):
+                    max_length=length
+                    longest_chain=chain
+        if longest_chain:
+            self.chain=longest_chain
+            return True
+        return False
 
 #Part-2 Mining on Blockchain
 # Creating a web app
 app=Flask(__name__)      
+
+#Creating an address for node on port 5000
+node_address=str(uuid4()).replace('-','')
     
 #Creating a blockchain
 blockchain=Blockchain()
@@ -82,12 +107,14 @@ def mine_block():
     previous_proof=previous_block['proof']
     proof=blockchain.proof_of_work(previous_proof)
     previous_hash=blockchain.hash(previous_block)
+    blockchain.add_transactions(sender=node_address,receiver='Kira',amount=1)     #Sender who is sending the btc receiver you are recieving reward and reward is amount
     block=blockchain.create_block(proof,previous_hash)
     response={'message':'Congratulation you just mined a block',
               'index':block['index'],
               'timestamp':block['timestamp'],
               'proof':block['proof'],
-              'previous_hash':block['previous_hash']
+              'previous_hash':block['previous_hash'],
+              'transactions':block['transactions']
             
             }
     return jsonify(response),200
@@ -110,15 +137,43 @@ def is_valid():
         response={'message':'We have a problem.The blockchain is not valid'}
     return jsonify(response),200
 
+@app.route('/add_transaction',methods=['POST'])
+def add_transaction():
+    json=request.get_json()                                                           #return the postman request in json format
+    transaction_keys=['sender','receiver','amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Some elemetns of transactions are missing',400
+    index=blockcain.add_transaction(json['sender'],json['receiver'],json['amount'])
+    response={'message':f'This transaction will be added to Block {index}'}
+    return jsonify(response),201
+
+#Connecting new nodes
+@app.route('/connect_node',method=['POST'])
+def connect_node():
+    json=request.get_json()
+    nodes=json.get('nodes')                                                         # "nodes"=["http://127.0.0.1:5001","http://127.0.0.1:5002","http://127.0.0.1:5003"]
+    if nodes is None:
+        return 'No node',400
+    for node in nodes:
+        blockchain.add_node(node)
+    response={'message':'All the nodes are now connected ,The kitcoin  Blockchain now contains the:',
+              'total_nodes':list(blockchain.nodes)}
+    return jsonify(response),201
+
+#Replacing the chain by the longest chain if needed
+@app.route('/replace_chain',method=['GET'])
+def replace_chain():
+    is_chain_replaced=blockchain.replace_chain()
+    if is_chain_replaced:
+        response={'message':'The nodes had different chains so the chain was replaced by the longest chain',
+                  'new_chain':blockchain.chain}
+    else:
+        response={'message':'All good ,The chain is the largest one ',
+                  'actual_chain':blockchain.chain}
+    return jsonify(response),200
+
+
+
 
 #Running the application
 app.run(host='0.0.0.0',port=5000)
-    
-
-
-
-
-
-
-
-
