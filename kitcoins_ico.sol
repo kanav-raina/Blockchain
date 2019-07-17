948daf3cb846d706f7aee05d4b3224cabfbf51f5
KIRA <kanavraina98@gmail.com>
2019-07-17 08:44:59 +0530

    smart contract file added from etherium remix ide

--- kitcoins_ico.sol
+++ kitcoins_ico.sol
@@ -7,7 +7,7 @@ contract_kitcoin_ico{
     uint public max_kitcoins=1000000;
     
     //Introducing USD to kitcoins
-    uint public usd_to_kitcoin=1000;
+    uint public usd_to_kitcoins=1000;
     
     //Introducing the total number of Kitcoins bought by investor
     uint public total_kitcoin_bought=0;
@@ -23,4 +23,34 @@ modifier can_buy_kitcoins(uint usd_invested){
  require(usd_invested*usd_to_kitcoins+total_kitcoins_bought<=max_kitcoins);
  _;
  
+}
+
+//Getting the equity in kitcoins of an investor
+function equity_in_kitcoins(address investor) external constant returns(uint){
+    return equity_kitcoins(investor);
+}
+
+
+//Getting the equity in USD of  an investor
+function equity in usd(address investor) external constant returns(uint){
+    return equity_usd(investor);
+}
+
+//Buying kitcoins
+function buy_kitcoins(address investor,uint usd_invested) external
+can_buy_kitcoins(usd_invested)
+{
+    uint kitcoins_bought=usd_invested*usd_to_kitcoins;
+    equity_kitcoins[investor]+=kitcoins_bought;
+    equity_usd[investor]=equity_kitcoins[investor]/1000;
+    total_kitcoins_bought+=kitcoins_bought;
+    
+}
+
+//Selling kitcoins_bought
+function sell_kitcoins(address investor,uint kitcoins_sold) external {
+    equity_kitcoins[investor]-=kitcoins_sold
+    equity_usd[investor]=equity_kitcoins[investor]/1000;
+    total_kitcoins_bought+=kitcoins_sold;
+    
 }
